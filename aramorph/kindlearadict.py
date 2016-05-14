00bf5e57c67e9dc9a35e7b7471bc753a047e06ab
Rune Holm <runehol@ping.uio.no>
2016-05-14 11:46:20 +0100

    many more cutoffs to speed up the process. now pretty decent

--- aramorph/kindlearadict.py
+++ aramorph/kindlearadict.py
@@ -15,6 +15,9 @@ ab = defaultdict(list)
 bc = defaultdict(list)
 ac = defaultdict(list)
 
+prefixes_for_cat = defaultdict(list)
+suffixes_for_cat = defaultdict(list)
+
 
 def process_prefixes():
     for (unvowelled, vowelled, cat, pos, gloss, root, lemma) in process_textfile("dictprefixes.txt"):
@@ -35,11 +38,17 @@ def process_suffixes():
 def process_tableAB():
     for (left, right) in process_tableXY("tableab.txt"):
         ab[left].append(right)
+        for entry in prefixes:
+            if entry.cat == left:
+                prefixes_for_cat[right].append(entry)
              
 
 def process_tableBC():
     for (left, right) in process_tableXY("tablebc.txt"):
         bc[left].append(right)
+        for entry in suffixes:
+            if entry.cat == right:
+                suffixes_for_cat[left].append(entry)
 
 def process_tableAC():
     for (left, right) in process_tableXY("tableac.txt"):
@@ -63,29 +72,33 @@ def gen_dict():
     process_tableBC()
     process_tableAC()
 
+    prefix_suffix_table = defaultdict(list)
+    for stem_cat, prefixes_list in prefixes_for_cat.iteritems():
+        for prefix_entry in prefixes_list:
+            for suffix_entry in suffixes_for_cat[stem_cat]:
+                if not are_prefix_suffix_compatible(prefix_entry, suffix_entry): continue
+                prefix_suffix_table[stem_cat].append( (prefix_entry, suffix_entry) )
+                
+
+    
     print len(stems), len(prefixes), len(suffixes)
 
-    stem_selection = stems[:100]
+    stem_selection = stems[:1000]
     n_generated = 0
     start_time = time.clock()
     for lemma, stem_list in stem_selection:
         for stem_entry in stem_list:
-            for prefix_entry in prefixes:
-                if not are_prefix_stem_compatible(prefix_entry, stem_entry): continue
+            for prefix_entry, suffix_entry in prefix_suffix_table[stem_entry.cat]:
 
-                for suffix_entry in suffixes:
-                    if not are_stem_suffix_compatible(stem_entry, suffix_entry): continue
-                    if not are_prefix_suffix_compatible(prefix_entry, suffix_entry): continue
-
-                    vowelled_form = prefix_entry.vowelled + \
+                vowelled_form = prefix_entry.vowelled + \
                                     stem_entry.vowelled + \
                                     suffix_entry.vowelled
 
-                    gloss = stem_entry.gloss
+                gloss = stem_entry.gloss
 
-                    uvowelled = transliterate.b2u(vowelled_form)
-#                    ala = transliterate.b2ala(vowelled_form)
-                    n_generated += 1
+                uvowelled = transliterate.b2u(vowelled_form)
+                n_generated += 1
+
 
     elapsed_time = time.clock() - start_time
     print "Generated %d forms from %d lemmas in %f seconds" % (n_generated, len(stem_selection), elapsed_time)
